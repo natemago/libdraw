1ff288733f9b85265fb3a8d75d1dc98fb4d0de05
Pavle Jonoski <jonoski.pavle@gmail.com>
2012-01-18 22:00:14 +0100

    example

--- tests/ld03002.js
+++ tests/ld03002.js
@@ -6,8 +6,8 @@
          canvas: data.canvas1 
       },
       clock:{
-         interval: 1000/100//,
-         //mode: 'interval'
+         interval: 1000/60,
+         mode: 'interval'
       }
    });
 
@@ -18,10 +18,11 @@
    var prevFps = 0;
    var fpscolor = undefined;
    shiftLeft = false;
+   stop = false;
    r.register(function(g, frame, rt){
-     /* g.background('black');
-      
-
+      //g.background('black');
+      g.clear(0,0,400,350);
+     
       var ms = g.millis();
       var ss = g.seconds() + (ms%1000)/1000;
       var mm = g.minutes()*60 + ss;
@@ -56,7 +57,7 @@
       var pm = g.minutes() < 10 ? '0' + g.minutes() : g.minutes();
       var ps = g.seconds() < 10 ? '0' + g.seconds() : g.seconds();
       g.text(ph + ':' + pm + ':' + ps, 145, 210 );
-      */
+      
       var n = Math.round(Math.random()*300000);
       //while(n--){
          
@@ -70,24 +71,33 @@
          }
          //g.fill(fpscolor);
          g.fill('cyan');
-         g.setFont('10px mono');
+         
          //g.text(fps.toFixed(2)+'fps', 20,20);
          var usage = r.clock.getMeasure().usage.usage;
-         g.ctx.clearRect(399,0,1,400 - (usage*400));
-         g.rect(399,400 - (usage*400),1,(usage*400));
-         //g.translate(1,0);
-         g.ctx.putImageData(g.ctx.getImageData(0,0,400,400), -1,0);
+         usage = usage*50;
+         g.strokeSize(0);
+         g.clear(399,0,1,400);
+         g.rect(399,400 - usage,1,usage);
+         
+         
+         
+         g.ctx.putImageData(g.ctx.getImageData(0,350,400,50), -1,350);
 
          prevFps = fps;
          shiftLeft = false;
+         
+      }
+      if(fps){
+         g.setFont('10px mono');
+         var usage = r.clock.getMeasure().usage.usage;
          g.ctx.clearRect(0,0,150,30);
          g.text(fps.toFixed(2) + 'fps : ' + (usage*100).toFixed(2) + '%', 2,20);
       }
-      
    });
    anotherClock = new libDraw.pkg.timer.Clock({interval: 1000});
-   anotherClock.addHandler(function(){
+   anotherClock.addHandler(function(tick){
       shiftLeft = true;
+     
    });
    
    anotherClock.start();
